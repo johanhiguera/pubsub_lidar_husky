f368b91f2069be8bdaeccfd94c23502a328d5b46
jmfajardod <jmfajardod@unal.edu.co>
2020-09-19 19:34:10 -0500

    Fixed error of copy method by importing numpy

--- scripts/class_pubsub.py
+++ scripts/class_pubsub.py
@@ -3,6 +3,8 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 
+import numpy as np
+from rospy.numpy_msg import numpy_msg 
 
 class LIDAR_RYCSV:
     
@@ -13,10 +15,10 @@ class LIDAR_RYCSV:
         self.newMsg = LaserScan() # LaserScan msg
 
         # Subscribers
-        rospy.Subscriber(self.nameTopicSub, LaserScan, self.Lidar_Callback, queue_size=10 )
+        rospy.Subscriber(self.nameTopicSub, numpy_msg(LaserScan), self.Lidar_Callback, queue_size=10 )
 
         # Publisher
-        self.pub = rospy.Publisher(self.nameTopicPub, LaserScan, queue_size=10)
+        self.pub = rospy.Publisher(self.nameTopicPub, numpy_msg(LaserScan), queue_size=10)
 
         # Polling con callback
         rate = rospy.Rate(20) # 20 Hz
@@ -29,10 +31,12 @@ class LIDAR_RYCSV:
     #--------------------------------------------------------------------------------------#
     # Callback o interrupcion
     def Lidar_Callback(self, lidar_scan):
-
-        new_msgLaserScan = lidar_scan.copy()
+        
+        new_msgLaserScan = lidar_scan
+        new_msgRanges    = lidar_scan.ranges.copy()
 
         #new_msgLaserScan.ranges > 10 m   == 0
         #new_msgLaserScan.ranges < 0.5 m  == 0
 
+        new_msgLaserScan.ranges = new_msgRanges
         self.newMsg = new_msgLaserScan
